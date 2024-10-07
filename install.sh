8164a9fadf2662ed3e9c67c31c55e719a4036ec1
RoninInPC <62109908+RoninInPC@users.noreply.github.com>
2024-10-07 18:09:15 +0500

    install.sh модификация

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ export PATH=$PATH:/usr/local/go/bin &&
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main . &&
 docker build -t bot_docker .
 
-cp -r service /etc/systemd/system/ &&
+cd service &&
+cp bot.service /etc/systemd/system/ &&
+cp redis.service /etc/systemd/system &&
 cd /etc/systemd/system &&
 systemctl start redis.service && systemctl start bot.service
 
