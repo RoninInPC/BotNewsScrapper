7bdaf632adf2ee68b856ddae9780ed7c5cb71c4e
RoninInPC <mrjaver@mail.ru>
2024-11-02 12:37:32 +0500

    РСЃРїСЂР°РІР»РµРЅРёРµ РґРµРїР»РѕСЏ

--- htmlgetter/withbrowser/withbrowser.go
+++ htmlgetter/withbrowser/withbrowser.go
@@ -101,9 +101,10 @@ func (h WithBrowser) GetScreenshot(url string) ([]byte, error) {
 func ReInstall() {
 	if isInstalled.Load() == true {
 		isInstalled.Store(false)
-		ans, _ := exec.Command("npx", "playwright", "uninstall").Output()
+		ans, _ := exec.Command("npx", "playwright", "uninstall", "---all").Output()
 		log.Println(string(ans))
-		_ = playwright.Install()
+		ans, _ = exec.Command("npx", "playwright", "install", "--with-deps").Output()
+		log.Println(string(ans))
 		isInstalled.Store(true)
 	}
 }
