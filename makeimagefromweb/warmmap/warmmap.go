45ce71f854cb17abfb9f19643bafaade0d1411d5
RoninInPC <mrjaver@mail.ru>
2024-11-13 10:29:06 +0500

    Подправка warmmap

--- makeimagefromweb/warmmap/warmmap.go
+++ makeimagefromweb/warmmap/warmmap.go
@@ -18,7 +18,7 @@ const (
 	x0       = 3
 	y0       = 70
 	x1       = 1279
-	y1       = 765
+	y1       = 790
 	FileName = "warmmap.png"
 	BaseURL  = "https://ru.tradingview.com/heatmap/stock/#%7B%22dataSource%22%3A%22MOEXRUSSIA%22%2C%22blockColor%22%3A%22change%22%2C%22blockSize%22%3A%22market_cap_basic%22%2C%22grouping%22%3A%22sector%22%7D"
 )
